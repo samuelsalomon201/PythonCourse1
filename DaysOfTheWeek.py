73c459b2bb92eef6b4fb4701d16e7e9e5cb37342
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-04 15:10:49 -0500

    OOOOOF But LOL

--- DaysOfTheWeek.py
+++ DaysOfTheWeek.py
@@ -1,1 +1,15 @@
-
+num=int(input("Enter the number:"))
+if(num==1):
+    print("Sunday")
+elif("num==2"):
+    print("Monday")
+elif(num==3):
+    print("Tuesday")
+elif (num == 4):
+    print("Wensday")
+elif(num==5):
+    print("Thrusday")
+elif(num==6):
+    print("Friday")
+elif(num==7):
+    print("Saturday")
