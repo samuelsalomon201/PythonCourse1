c5dc2dc869db42d4b250aed056f6f9e5d64a4394
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-05 10:42:36 -0500

    Spam 1 2BOOM

---
+++ Looping Conditions.py
@@ -0,0 +1,5 @@
+ch=1
+while ch==1:
+    print("I Love Fortnite<3 Spam 1 2BOOM")
+    ch=int(input())
+print("GoodBye")
---
+++ MoreLooping.py
@@ -0,0 +1,5 @@
+n=int(input("Enter Number WARNING DON'T MULTIPLY BY 100000000000000 WILL MAKE SOMETHING FUNKY HAPPEN:"))
+for i in range(0,n):
+    for j in range (0,i+1):
+        print("*\t",end="")
+    print()
