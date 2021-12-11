4b733caa21541f8c143fe625e38423a4fc616e91
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-11 14:29:11 -0500

    Made More Set Methods

---
+++ AnotherSetMethod.py
@@ -0,0 +1,6 @@
+s1={1,2,3,4,5}
+s2={2,3,6,7}
+
+#s1 union s2 = {1,2,3,4,5,6,7}
+#print(s1.union(s2)) = {1,4,5,6,7}
+print(s1.symmetric_difference(s2))
---
+++ SetMethods.py
@@ -0,0 +1,5 @@
+print("OOF")
+s={1,2,3,4,5}
+s.clear()
+s.add(6)
+print(s)
