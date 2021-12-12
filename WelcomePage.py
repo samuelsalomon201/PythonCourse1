478b9a90e286eec9823d0f1e93297e4f331a3138
samuelsalomon201 <84681310+samuelsalomon201@users.noreply.github.com>
2021-12-12 14:43:21 -0500

    ADdvanced Some More

---
+++ WelcomePage.py
@@ -0,0 +1,25 @@
+for i in range(0,20):
+    print("*\t",end="")
+print()
+print("*",end="")
+for i in range(0,19):
+    print("\t",end="")
+print("*")
+print("*",end="")
+for i in range(0,19):
+    print("\t",end="")
+print("*")
+print("*",end="")
+for i in range(0,10):
+    print("\t",end="")
+print("WELCOME")
+
+
+
+'''
+*   *   *   *   *
+*               *
+*    welcome    *
+*   *   *   *   *
+
+press any key to start'''
